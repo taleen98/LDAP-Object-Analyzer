3321ee2a4937b702b43aa4153c3bb4a082201346
T00n <68474047+taleen98@users.noreply.github.com>
2024-10-29 16:28:16 +0200

    Update ldap.sh

--- ldap.sh
+++ ldap.sh
@@ -8,7 +8,6 @@ YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
 RESET='\033[0m' # Reset color to default
 
-
 figlet -f slant "LDAP ENUM" | lolcat
 echo -e "          by t00n${RESET}" | lolcat
 echo 
@@ -21,7 +20,7 @@ echo -n "Your 1st Part Of Your Domain Name: "; read dc1
 echo -n "Your 2nd Part Of Your Domain Name: "; read dc2
 echo -n "Your Domain Controller IP: "; read DCIP
 
-ldap_result=$(ldapsearch -x -b "DC=$dc1,DC=$dc2" -H ldap://$DCIP )
+ldap_result=$(ldapsearch -x -b "DC=$dc1,DC=$dc2" -H ldap://$DCIP)
 
 echo -e "${GREEN}LDAP Search Results:${RESET}"
 echo "$ldap_result" | awk -v BLUE="${BLUE}" -v CYAN="${CYAN}" -v RESET="${RESET}" '
@@ -62,7 +61,6 @@ echo "$ldap_result" | awk -v BLUE="${BLUE}" -v CYAN="${CYAN}" -v RESET="${RESET}
             print CYAN "This object is a group:" RESET
             print CYAN "sAMAccountName: " sAMAccountName RESET
             isGroup = 0
-            sAMAccountName = ""
             if (dn != "") {
                 print dn > "DNs.txt"
             }
@@ -71,14 +69,31 @@ echo "$ldap_result" | awk -v BLUE="${BLUE}" -v CYAN="${CYAN}" -v RESET="${RESET}
         print "---"  # Separating line between objects
     }
 '
-# sort the file DNs.txt from repetition
+
+# Sort the file DNs.txt from repetition
 sort -u -o "DNs.txt" "DNs.txt"
 
-# Collect Description values in a file called Desc.txt 
-ldapsearch -H ldap://$DCIP  -x -b "DC=$dc1,DC=$dc2" "objectClass=*" description | grep "description:" > Description.txt
-
+# Collect descriptions with sAMAccountName in a file called Description.txt
+> Description.txt  # Clear the file before appending
+ldapsearch -H ldap://$DCIP -x -b "DC=$dc1,DC=$dc2" "(objectClass=*)" sAMAccountName description | awk '
+    BEGIN {
+        sAMAccountName = "";
+        description = "";
+    }
+    /^sAMAccountName:/ { sAMAccountName = $2; }
+    /^description:/ { description = $2; }
+    /^$/ {
+        if (sAMAccountName && description) {
+            print "sAMAccountName: " sAMAccountName ", description: " description;
+        }
+        sAMAccountName = "";  # Reset for the next entry
+        description = "";     # Reset for the next entry
+    }
+' >> Description.txt
 
 # Now, iterate through DNs in DNs.txt and check groupType
+> DomainGroups.txt  # Create or clear DomainGroups.txt
+> LocalGroups.txt   # Create or clear LocalGroups.txt
 
 while IFS= read -r dn; do
     groupType_result=$(ldapsearch -x -b "$dn" -H ldap://$DCIP -s base groupType)
@@ -86,14 +101,19 @@ while IFS= read -r dn; do
         # Print the sAMAccountName of Domain Group
         sAMAccountName=$(ldapsearch -x -b "$dn" -H ldap://$DCIP -s base sAMAccountName | awk '/sAMAccountName:/{print}')
         echo -e "${GREEN}This group is a Domain Group:${RESET} ${YELLOW}$sAMAccountName${RESET}"
+        echo "$sAMAccountName" >> DomainGroups.txt
     elif [[ "$groupType_result" == *"groupType: -2147483644"* ]]; then
         # Print the sAMAccountName of Local Group
         sAMAccountName=$(ldapsearch -x -b "$dn" -H ldap://$DCIP -s base sAMAccountName | awk '/sAMAccountName:/{print}')
         echo -e "${GREEN}This group is a Local Group:${RESET} ${RED}$sAMAccountName${RESET}"
+        echo "$sAMAccountName" >> LocalGroups.txt
     else
         echo -e "${RED}Error: Unable to determine the group type for DN: $dn${RESET}"
     fi
 done < "DNs.txt"
+
 echo 
 echo -e "${GREEN}All Found Usernames Have Been Saved To File${RESET} ${BOLD_GREEN} Users.txt${RESET}"
-echo -e "${GREEN}All Found Users' Description Have Been Saved To File${RESET} ${BOLD_GREEN}Description.txt${RESET}"
+echo -e "${GREEN}All Found Users' Descriptions Have Been Saved To File${RESET} ${BOLD_GREEN} Description.txt${RESET}"
+echo -e "${GREEN}All Domain Groups Have Been Saved To File${RESET} ${BOLD_GREEN} DomainGroups.txt${RESET}"
+echo -e "${GREEN}All Local Groups Have Been Saved To File${RESET} ${BOLD_GREEN} LocalGroups.txt${RESET}"
